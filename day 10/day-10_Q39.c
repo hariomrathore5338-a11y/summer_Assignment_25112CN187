a7f620c7182faadf25de6f75816f4f265cc1aaec
Hari om Rathore <hariomrathore5338@gmail.com>
2026-07-14 04:02:56 +0530

    Add files via upload

---
+++ day 10/day-10_Q39.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        // spaces
+        for (int j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+        // increasing numbers
+        for (int j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+        // decreasing numbers
+        for (int j = i - 1; j >= 1; j--) {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}

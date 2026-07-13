1ec1ccf5e0049c43d64515d0b93ba747f226caaa
Hari om Rathore <hariomrathore5338@gmail.com>
2026-07-14 04:01:48 +0530

    Add files via upload

---
+++ day 7/DAY-7_Q28.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int reverse(int n, int rev) {
+    if (n == 0) return rev;   // base case
+    return reverse(n / 10, rev * 10 + (n % 10));
+}
+
+int main() {
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    printf("Reversed number = %d\n", reverse(n, 0));
+    return 0;
+}

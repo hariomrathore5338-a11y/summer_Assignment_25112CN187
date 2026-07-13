5c3062a72dc1a69f68b4cb01977b78f90d07dcfb
Hari om Rathore <hariomrathore5338@gmail.com>
2026-07-14 04:05:25 +0530

    Add files via upload

---
+++ day 18/day-18_Q72.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void sortDescending(int arr[], int n) {
+    for (int i = 0; i < n-1; i++) {
+        for (int j = i+1; j < n; j++) {
+            if (arr[i] < arr[j]) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int arr[] = {5, 2, 9, 1, 5, 6};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    sortDescending(arr, n);
+
+    printf("Array sorted in descending order:\n");
+    for (int i = 0; i < n; i++) printf("%d ", arr[i]);
+    return 0;
+}

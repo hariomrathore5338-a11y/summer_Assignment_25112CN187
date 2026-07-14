95a976f7a3a5a0c44fb819880ae309cb1b8f2277
Hari om Rathore <hariomrathore5338@gmail.com>
2026-07-14 16:58:52 +0530

    Add files via upload

---
+++ day 25/day-25_Q99.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char names[10][50], temp[50];
+    int n;
+    printf("Enter number of names: ");
+    scanf("%d", &n);
+
+    for (int i=0; i<n; i++) scanf("%s", names[i]);
+
+    for (int i=0; i<n-1; i++) {
+        for (int j=i+1; j<n; j++) {
+            if (strcmp(names[i], names[j]) > 0) {
+                strcpy(temp, names[i]);
+                strcpy(names[i], names[j]);
+                strcpy(names[j], temp);
+            }
+        }
+    }
+
+    printf("Sorted names: \n");
+    for (int i=0; i<n; i++) printf("%s\n", names[i]);
+    return 0;
+}
